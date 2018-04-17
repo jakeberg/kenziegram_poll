68b132e251f272210e1691b229797ff7e9da299c
Jake Berg <jacobvictorberg@gmail.com>
2018-04-17 15:38:42 -0400

    added pug

--- index.js
+++ index.js
@@ -9,68 +9,33 @@ const app = express();
 
 const uploaded_files = [];
 
+app.set('view engine', 'pug');
+app.set('views', './public/views')
 app.use(express.static('public'));
 app.use(express.static('./public/uploads/'));
+
 
 app.get('/photos', function (req, res) {
     res.statusCode = 200;
     const path = './public/uploads';
 
     fs.readdir(path, function (err, items) {
-        let content = '';
-        for (var i = 0; i < items.length; i++) {
-            content += `<img src='${items[i]}' alt='pic'>`;
-        }
-
-        res.send(`
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Kenziegram</title>
-    </head>
-    <body>
-    <header><h1>KENZIEGRAM</h1></header>
-    <form action="http://localhost:3000/upload" method="post" enctype="multipart/form-data">
-            
-                <input type="file" id="file" name="myFile" value="file">
-                <input type="submit" class="button" id="upload" value="upload">
-           
-        </form>
-        <div class="content">
-         ${content}
-         <div>
-    </body>
-        `);
-
+        res.render('index', {
+            title: 'Kenziegram',
+            message: 'Kenziegram',
+            url: items
+        });
     });
 });
 
 app.post('/upload', upload.single('myFile'), function (req, res, next) {
-    // req.file is the `myFile` file
-    // req.body will hold the text fields, if there were any
-    console.log("Uploaded: " + req.file.filename);
-    
+
     uploaded_files.push(req.file.filename);
-    res.end(`
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Kenziegram</title>
-    </head>
-    <body>
-    <header><h1>KENZIEGRAM</h1></header>
-    <div class="content">
-    <h1>File Uploaded!</h1>
-    <a href='http://localhost:3000/photos'>Click to go to your photos
-    <img src='${req.file.filename}' alt='pic'"></a>
-    </div>
-    </body>`);
+    res.render('upload', {
+        title: 'Kenziegram',
+        message: 'Kenziegram',
+        image: req.file.filename
+    });
 })
-
-
 
 app.listen(port);
